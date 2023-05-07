f6bdbac3d08e1a7dc7a7ad8cd1ade3bd5dda5406
AntonioTorga <antonio.torga@ug.uchile.cl>
2023-05-07 17:46:29 -0400

    paths ready, transform to flask ready

--- app.py
+++ app.py
@@ -7,14 +7,27 @@ app = Flask(__name__)
 def inicio():
     return render_template("inicio.html")
 
-# @app.route("/donar")
+@app.route("/donar")
+def donar():
+    return render_template("agregar/agregar-donacion.html")
 
-# @app.route("/pedir")
+@app.route("/pedir")
+def pedir():
+    return render_template("agregar/agregar-pedido.html")
 
-# @app.route("/ver-donaciones")
+@app.route("/ver-donaciones")
+def verDonaciones():
+    return render_template("ver/ver-donaciones.html")
 
-# @app.route("/ver-pedidos")
+@app.route("/ver-pedidos")
+def verPedidos():
+    return render_template("ver/ver-pedidos.html")
 
-# @app.route("/info-donacion")
+@app.route("/info-donacion")
+def infoDonacion():
+    return render_template("info/informacion-donacion.html")
 
-# @app.route("/info-pedido")
+@app.route("/info-pedido")
+def infoPedido():
+    return render_template("info/informacion-pedido.html")
+
